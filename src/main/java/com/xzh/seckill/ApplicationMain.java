1c8807c1466fedbb41f5da82d18314e8673b0c2b
xzh <anenan@qq.com>
2019-10-30 09:33:22 +0800

    first commit

---
+++ src/main/java/com/xzh/seckill/ApplicationMain.java
@@ -0,0 +1,19 @@
+package com.xzh.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动
+ *
+ * @author: 向振华
+ * @date: 2019/10/30 10:56
+ */
+@SpringBootApplication
+public class ApplicationMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class, args);
+    }
+
+}
